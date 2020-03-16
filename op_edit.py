a39a7698a3574f3d499f469b93d7f8981a36260f
Shane Smiskol <shane@smiskol.com>
2020-03-15 20:14:09 -0500

    Add Discord username prompt to op_edit for sentry debugging

--- op_edit.py
+++ op_edit.py
@@ -9,46 +9,53 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
     self.op_params = opParams()
     self.params = None
     self.sleep_time = 1.0
-    self.live_tuning = False
-    self.welcome()
+    self.live_tuning = self.op_params.get('op_edit_live_mode', False)
+    self.username = self.op_params.get('username', None)
 
-  def welcome(self):
-    print('Welcome to the opParams command line editor!')
-    while True:
-      print('Would you like to enter opEdit\'s live-tuning mode?')
-      choice = input('[Y/n]: ').lower().strip()
-      if choice in ['y', 'ye', 'yes']:
-        self.live_tuning = True
-        self.run_loop()
-        break
-      elif choice in ['n', 'no']:
-        self.run_loop()
-        break
-      elif choice in ['e', 'exit']:
-        break
+    self.run_init()
+
+  def run_init(self):
+    if self.username is None:
+      print('\nWelcome to the opParams command line editor!')
+      print('Parameter \'username\' is missing! Would you like to add your Discord username for easier crash debugging?')
+      if self.is_affirmative():
+        print('Please enter your Discord username so the developers can reach out if a crash occurs:')
+        username = ''
+        while username == '':
+          username = input('>> ').strip()
+        self.message('Thanks! Saving your Discord username to op_params.json\n'
+                     'Edit the \'username\' parameter at any time to update', sleep_time=3.0)
+        self.op_params.put('username', username)
+        self.username = username
+    else:
+      print('\nWelcome to the opParams command line editor, {}!'.format(self.username))
+
+    self.run_loop()
 
   def run_loop(self):
-    if not self.live_tuning:
-      print('Here are your parameters:\n')
-    else:
-      print('Here are your live parameters:\n')
     while True:
+      if not self.live_tuning:
+        print('Here are your parameters:\n')
+      else:
+        print('Here are your live parameters:\n')
       self.params = self.op_params.get(force_update=True)
       if self.live_tuning:  # only display live tunable params
-        self.params = {k: v for k, v in self.params.items() if self.op_params.get_key_info(k).live}
+        self.params = {k: v for k, v in self.params.items() if self.op_params.key_info(k).live}
 
       values_list = [self.params[i] if len(str(self.params[i])) < 20 else '{} ... {}'.format(str(self.params[i])[:30], str(self.params[i])[-15:]) for i in self.params]
-      live = ['(live!)' if self.op_params.get_key_info(i).live else '' for i in self.params]
+      live = ['(live!)' if self.op_params.key_info(i).live else '' for i in self.params]
 
       to_print = ['{}. {}: {}  {}'.format(idx + 1, i, values_list[idx], live[idx]) for idx, i in enumerate(self.params)]
-      to_print.append('\n{}. Add new parameter!'.format(len(self.params) + 1))
-      to_print.append('{}. Delete parameter!'.format(len(self.params) + 2))
+      extras = ['---\na. Add new parameter',
+                'd. Delete parameter',
+                'l. Toggle live tuning']
+      to_print += extras
 
       print('\n'.join(to_print))
-      print('\nChoose a parameter to explore (by integer index): ')
+      print('\nChoose a parameter to explore (by identifier): ')
 
       choice = input('>> ').strip()
-      parsed, choice = self.parse_choice(choice)
+      parsed, choice = self.parse_choice(choice, len(to_print) - len(extras))
       if parsed == 'continue':
         continue
       elif parsed == 'add':
@@ -57,35 +64,39 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
         self.change_parameter(choice)
       elif parsed == 'delete':
         self.delete_parameter()
-      elif parsed == 'error':
+      elif parsed == 'live':
+        self.live_tuning = not self.live_tuning
+        self.op_params.put('op_edit_live_mode', self.live_tuning)  # for next opEdit startup
+      elif parsed == 'exit':
         return
 
-  def parse_choice(self, choice):
+  def parse_choice(self, choice, opt_len):
     if choice.isdigit():
       choice = int(choice)
       choice -= 1
-    elif choice == '':
+      if choice not in range(opt_len):  # number of options to choose from
+        self.message('Not in range!')
+        return 'continue', choice
+      return 'change', choice
+
+    if choice == '':
       print('Exiting opEdit!')
-      return 'error', choice
-    else:
-      self.message('Not an integer!')
-      return 'retry', choice
-    if choice not in range(0, len(self.params) + 2):  # three for add/delete parameter
-      self.message('Not in range!')
-      return 'continue', choice
+      return 'exit', choice
 
-    if choice == len(self.params):  # add new parameter
+    if choice in ['a', 'add']:  # add new parameter
       return 'add', choice
-
-    if choice == len(self.params) + 1:  # delete parameter
+    elif choice in ['d', 'delete', 'del']:  # delete parameter
       return 'delete', choice
+    elif choice in ['l', 'live']:  # live tuning mode
+      return 'live', choice
 
-    return 'change', choice
+    self.message('Invalid choice!')
+    return 'continue', choice
 
   def change_parameter(self, choice):
     while True:
       chosen_key = list(self.params)[choice]
-      key_info = self.op_params.get_key_info(chosen_key)
+      key_info = self.op_params.key_info(chosen_key)
 
       old_value = self.params[chosen_key]
       print('Chosen parameter: {}'.format(chosen_key))
@@ -103,7 +114,7 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
         print('\n{}\n'.format('\n'.join(to_print)))
 
       print('Current value: {} (type: {})'.format(old_value, str(type(old_value)).split("'")[1]))
-      if key_info.live or self.live_tuning:  # similar to opTune
+      if key_info.live:  # enter live tuning interface
         while True:
           print('Enter your new value:')
           new_value = input('>> ').strip()
@@ -140,6 +151,9 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
         else:
           self.message('Not saved!')
         return
+
+  def is_affirmative(self):
+    return input('[Y/n]: ').lower().strip() in ['yes', 'ye', 'y']
 
   def parse_input(self, dat):
     dat = dat.strip()
@@ -211,9 +225,11 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
         self.message('Not saved!')
       return
 
-  def message(self, msg):
+  def message(self, msg, sleep_time=None):
+    if sleep_time is None:
+      sleep_time = self.sleep_time
     print('--------\n{}\n--------'.format(msg), flush=True)
-    time.sleep(self.sleep_time)
+    time.sleep(sleep_time)
     print()
 
 
